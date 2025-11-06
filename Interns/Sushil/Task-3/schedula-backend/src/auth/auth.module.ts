984e4fe26097b09c978a816c004e8efc8587a651
Sushil Khanna <sushil15sep2004@gmail.com>
2025-11-06 23:34:48 +0530

    Task 4: Implemented Authentication (Signup, Signin, Signout)

--- Interns/Sushil/Task-3/schedula-backend/src/auth/auth.module.ts
+++ Interns/Sushil/Task-3/schedula-backend/src/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PrismaClient } from '@prisma/client';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [
+    JwtModule.register({
+      secret: 'SECRET_KEY',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, PrismaClient, JwtStrategy],
 })
 export class AuthModule {}
--- Interns/Sushil/Task-3/schedula-backend/src/auth/auth.service.ts
+++ Interns/Sushil/Task-3/schedula-backend/src/auth/auth.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaClient, private jwt: JwtService) {}
+
+  async signup(email: string, password: string) {
+    const hashed = await bcrypt.hash(password, 10);
+    const user = await this.prisma.user.create({
+      data: { email, password: hashed },
+    });
+    return { message: 'User registered successfully', user };
+  }
+
+  async signin(email: string, password: string) {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+
+    if (!user) return { error: 'User not found' };
+
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) return { error: 'Incorrect Password' };
+
+    const token = this.jwt.sign({ sub: user.id, email: user.email });
+    return { message: 'Login successful', token };
+  }
+
+  async signout() {
+    return { message: 'Signout successful (handled on client side)' };
+  }
+}
